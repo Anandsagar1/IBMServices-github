f1f6d1ed44607cf2e8f7d9cade97141384a5e9f3
anand sagar <anand.sagar1@ibm.com>
2019-09-13 09:08:01 -0400

    commit

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,19 @@
+module.exports = [{
+      plugin: require('/Users/anand/Desktop/IBM-Services/Gatsby/IBMServices-github/node_modules/gatsby-plugin-catch-links/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/anand/Desktop/IBM-Services/Gatsby/IBMServices-github/node_modules/gatsby-plugin-lunr/gatsby-browser.js'),
+      options: {"plugins":[],"languages":[{"name":"en","plugins":[null]}],"fields":[{"name":"title","store":true,"attributes":{"boost":30}},{"name":"keywords"},{"name":"path","store":true},{"name":"description","store":true},{"name":"content"}],"resolvers":{"SitePage":{}}},
+    },{
+      plugin: require('/Users/anand/Desktop/IBM-Services/Gatsby/IBMServices-github/node_modules/gatsby-plugin-mdx/gatsby-browser.js'),
+      options: {"plugins":[{"resolve":"/Users/anand/Desktop/IBM-Services/Gatsby/IBMServices-github/node_modules/gatsby-remark-images","id":"3626c0e0-58d4-5046-b0e5-cb7731a052f2","name":"gatsby-remark-images","version":"3.1.21","pluginOptions":{"plugins":[]},"nodeAPIs":[],"browserAPIs":["onRouteUpdate"],"ssrAPIs":["onRenderBody"]}],"extensions":[".mdx",".md"],"gatsbyRemarkPlugins":[{"resolve":"gatsby-remark-unwrap-images"},{"resolve":"gatsby-remark-smartypants"},{"resolve":"gatsby-remark-images","options":{"maxWidth":1152,"linkImagesToOriginal":false,"quality":75,"withWebp":false}},{"resolve":"gatsby-remark-responsive-iframe"},{"resolve":"gatsby-remark-copy-linked-files"}],"remarkPlugins":[null],"defaultLayouts":{"default":"/Users/anand/Desktop/IBM-Services/Gatsby/IBMServices-github/node_modules/gatsby-theme-carbon/src/templates/Default.js","home":"/Users/anand/Desktop/IBM-Services/Gatsby/IBMServices-github/node_modules/gatsby-theme-carbon/src/templates/Homepage.js"}},
+    },{
+      plugin: require('/Users/anand/Desktop/IBM-Services/Gatsby/IBMServices-github/node_modules/gatsby-remark-images/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/anand/Desktop/IBM-Services/Gatsby/IBMServices-github/node_modules/gatsby-theme-carbon/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/anand/Desktop/IBM-Services/Gatsby/IBMServices-github/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
